1eceb941858c8463bb3cae1bf5858e48d658b313
nguyenvancongdev <72190917+nguyenvancongdev@users.noreply.github.com>
2022-05-26 14:02:47 +0700

    Update Menu.tsx

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 const Menu: NextPage = () => {
   return (
   <div>
-     <Link  href=""><a>Home</a></Link>
+     <Link  href="/"><a>Home</a></Link>
      <Link  href="/blog"><a>Blog</a></Link>
      <Link href="/hotocnguyenvan"><a>Ho toc nguyen van</a></Link>
      <Link href="/todo"><a>todo</a></Link>
