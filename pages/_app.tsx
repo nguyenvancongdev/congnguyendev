395725a7cb41490c1f74a2006acea0d8aa0a9876
nguyenvancongdev <72190917+nguyenvancongdev@users.noreply.github.com>
2022-05-26 09:21:20 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return
-  <>
-  đây là thanh menu
-  <Component {...pageProps} />
-  </>  
+  return <Component {...pageProps} />
 }
 
 export default MyApp
