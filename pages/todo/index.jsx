b93e54ae18f284b70ace988ca1cd447e172f884c
nguyenvancongdev <72190917+nguyenvancongdev@users.noreply.github.com>
2022-05-26 10:41:53 +0700

    Create index.jsx

---
+++ pages/todo/index.jsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Link from "next/link"
+
+const Todo: NextPage = () => {
+  return (
+  <div>
+    những việc cần làm
+  </div>
+  )
+}
+
+export default Todo
