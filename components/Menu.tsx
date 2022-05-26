dd02d6993c5d82be0c16d0170c7bbb70c77f84b7
nguyenvancongdev <72190917+nguyenvancongdev@users.noreply.github.com>
2022-05-26 10:37:41 +0700

    Create Menu.tsx

---
+++ components/Menu.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Link from "next/link"
+
+const Menu: NextPage = () => {
+  return (
+  <div>
+     <Link  href=""><a>Home</a></Link>
+     <Link  href="/blog"><a>Blog</a></Link>
+     <Link href="/hotocnguyenvan"><a>Ho toc nguyen van</a></Link>
+     <Link href="/todo"><a>Dang nhap</a></Link>
+  </div>
+  )
+}
+
+export default Menu
