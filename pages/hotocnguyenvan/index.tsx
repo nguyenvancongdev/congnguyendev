ad09a6da6d316256ecf0e04c08b3e854ed8f3445
nguyenvancongdev <72190917+nguyenvancongdev@users.noreply.github.com>
2022-05-26 14:14:55 +0700

    Create index.tsx

---
+++ pages/hotocnguyenvan/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Hotocnguyenvan: NextPage = () => {
+  return (
+  <div>
+   trang web cua ho toc nguyen van
+  </div>
+  )
+}
+
+export default Hotocnguyenvan
