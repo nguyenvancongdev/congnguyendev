3969408634bcb7019159f0de2592fd6334b5d1d5
nguyenvancongdev <72190917+nguyenvancongdev@users.noreply.github.com>
2022-05-27 09:30:51 +0700

    Create 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+const MyCustom404Page: NextPage = () => {
+  return (
+  <div>
+       <Head>
+          <title>Todo </title>
+      </Head>
+    những việc cần làm
+  </div>
+  )
+}
+
+export default MyCustom404Page
