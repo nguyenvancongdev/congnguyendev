4ce1b4de133b63ee4ed7fbe8272ae00d3e16d6c1
nguyenvancongdev <72190917+nguyenvancongdev@users.noreply.github.com>
2022-05-27 10:32:44 +0700

    Update index.tsx

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,1 +1,23 @@
 
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Công Nguyễn Developer </title>
+      </Head>
+
+      <main className={styles.main}>
+       đây là nội dung trang web
+      </main>
+
+     
+    </div>
+  )
+}
+
+export default Home
