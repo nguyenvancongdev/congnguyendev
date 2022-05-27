f9ad47ca9d8cb16a5f27207a496877dfc30f0983
nguyenvancongdev <congthang.nuce@gmail.com>
2022-05-27 21:36:26 +0700

    f

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,8 @@
 import 'styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-     <Head>
-          <title>Công Nguyễn Dev</title>
-          <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"/>
-      </Head>
-    <Component {...pageProps} />
-    </> 
-   )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
