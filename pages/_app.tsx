1fa1ec3e27c6dd1303ba458d6895dd04ce8362bb
nguyenvancongdev <72190917+nguyenvancongdev@users.noreply.github.com>
2022-05-27 10:04:44 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import Menu from '../components/Menu'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+     <Head>
+          <title>hello moi nguoi</title>
+          <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+      </Head>
     <Menu />
     <Component {...pageProps} />
     </> 
