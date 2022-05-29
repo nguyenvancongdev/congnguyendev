1a94385c4d08a1386847fb1030880e614d4c6413
nguyenvancongdev <congthang.nuce@gmail.com>
2022-05-29 12:09:26 +0700

    r

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-
-
+import { useRouter } from 'next/router'
 const MyCustom404Page: NextPage = () => {
+  const router = useRouter()
+  const routerGohomes = () => {
+    router.push('/')
+  } 
+  const routerContactUs = () => {
+    router.push('https://www.facebook.com/conguyendev')
+  } 
   return (
    <div>
        <Head>
@@ -15,12 +21,12 @@ const MyCustom404Page: NextPage = () => {
         <h1 className="text-9xl font-bold text-purple-400">404</h1>
         <h1 className="text-6xl font-medium py-8">oops! Page not found</h1>
         <p className="text-2xl pb-8 px-12 font-medium">Oops! The page you are looking for does not exist. It might have been moved or deleted.</p>
-        <button className="bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 text-white font-semibold px-6 py-3 rounded-md mr-6">
+        <button className="bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 text-white font-semibold px-6 py-3 rounded-md mr-6" onClick={routerGohomes}>
             
         HOME
 
         </button>
-        <button className="bg-gradient-to-r from-red-400 to-red-500 hover:from-red-500 hover:to-red-500 text-white font-semibold px-6 py-3 rounded-md">
+        <button className="bg-gradient-to-r from-red-400 to-red-500 hover:from-red-500 hover:to-red-500 text-white font-semibold px-6 py-3 rounded-md" onClick={routerContactUs}>
         Contact Us
         </button>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {useEffect} from 'react';
+import Blog from 'pages/blog'
 const Home: NextPage = () => {
   const router = useRouter()
-  useEffect(() => {
-    router.push('/todo')
-  });
   return (
-   <></>
+   <>
+   <Blog/>
+   </>
   )
 }
 
