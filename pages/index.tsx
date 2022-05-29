0a0fddeda35cbff1710576f3d15922b02555c482
nguyenvancongdev <congthang.nuce@gmail.com>
2022-05-29 11:30:13 +0700

    f

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import {useEffect} from 'react';
 const Home: NextPage = () => {
+  const router = useRouter()
+  useEffect(() => {
+    router.push('/todo')
+  });
   return (
-   <>hehehe</>
+   <></>
   )
 }
 
